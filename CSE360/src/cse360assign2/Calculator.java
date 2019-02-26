c0ecd3c03f10b2482020ca5c03771e302dcf26cd
Bradley Villegas <bbv1414@gmail.com>
2019-02-25 22:58:22 -0700

    Third commit

--- CSE360/src/cse360assign2/Calculator.java
+++ CSE360/src/cse360assign2/Calculator.java
@@ -11,6 +11,7 @@ package cse360assign2;
 public class Calculator {
 
 	private int total;
+	private String history = "0";
 
 	/**
 	 * Creates a calculator
@@ -27,49 +28,53 @@ public class Calculator {
 	}
 
 	/**
-	 * Adds a number to the total
+	 * Adds a number to the total and updates the history string accordingly
 	 * 
 	 * @param value
 	 * 			the integer to be added to the total
 	 */
 	public void add(int value) {
 		total = total + value;
+		history = history + " + " + value;
 	}
 
 	/**
-	 * Subtracts a number from the total
+	 * Subtracts a number from the total and updates the history string accordingly
 	 * 
 	 * @param value
 	 * 			the integer to be subtracted from the total
 	 */
 	public void subtract(int value) {
 		total = total - value;
+		history = history + " - " + value;
 	}
 
 	/**
-	 * Multiplies a number by the total
+	 * Multiplies a number by the total and updates the history string accordingly
 	 * 
 	 * @param value
 	 * 			the integer to be multiplied by the total
 	 */
 	public void multiply(int value) {
 		total = total * value;
+		history = history + " * " + value;
 	}
 
 	/**
-	 * Divides the total by the number
+	 * Divides the total by the number and updates the history string accordingly
 	 * 
 	 * @param value
 	 * 			the integer to divide into the total
 	 */
 	public void divide(int value) {
 		total = total / value;
+		history = history + " / " + value;
 	}
 
 	/**
 	 * Returns the history of the calculations
 	 */
 	public String getHistory() {
-		return "";
+		return history;
 	}
 }
--- CSE360/src/cse360assign2/CalculatorMain.java
+++ CSE360/src/cse360assign2/CalculatorMain.java
@@ -4,6 +4,13 @@ import cse360assign2.Calculator;
 
 public class CalculatorMain {
 	public static void main(String[] args) {
-		
+		Calculator calc = new Calculator();
+		calc.add(3);
+		calc.subtract(1);
+		calc.multiply(4);
+		calc.divide(2);
+		System.out.println(calc.getTotal());
+		System.out.println();
+		System.out.println(calc.getHistory());
 	}
 }
