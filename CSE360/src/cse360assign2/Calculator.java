7c51b09ec11c2c5efdd17d01b31ca77c5ece2a08
Bradley Villegas <bbv1414@gmail.com>
2019-02-25 21:51:18 -0700

    Initial commit

---
+++ CSE360/src/cse360assign2/Calculator.java
@@ -0,0 +1,75 @@
+/** 
+* The Calculator class contains methods of different calculator
+* functions. It can add, subtract, multiply, divide, and list
+* the number history.
+* 
+* @author Bradley Villegas Class ID: 576
+*/
+
+package cse360assign2;
+
+public class Calculator {
+
+	private int total;
+
+	/**
+	 * Creates a calculator
+	 */
+	public Calculator() {
+		total = 0; // not needed - included for clarity
+	}
+
+	/**
+	 * Returns the total
+	 */
+	public int getTotal() {
+		return 0;
+	}
+
+	/**
+	 * Adds a number to the total
+	 * 
+	 * @param value
+	 * 			the integer to be added to the total
+	 */
+	public void add(int value) {
+
+	}
+
+	/**
+	 * Subtracts a number from the total
+	 * 
+	 * @param value
+	 * 			the integer to be subtracted from the total
+	 */
+	public void subtract(int value) {
+
+	}
+
+	/**
+	 * Multiplies a number by the total
+	 * 
+	 * @param value
+	 * 			the integer to be multiplied by the total
+	 */
+	public void multiply(int value) {
+
+	}
+
+	/**
+	 * Divides the total by the number
+	 * 
+	 * @param value
+	 * 			the integer to divide into the total
+	 */
+	public void divide(int value) {
+
+	}
+
+	/**
+	 * Returns the history of the calculations
+	 */
+	public String getHistory() {
+		return "";
+	}
+}
---
+++ CSE360/src/cse360assign2/CalculatorMain.java
@@ -0,0 +1,9 @@
+package cse360assign2;
+
+import cse360assign2.Calculator;
+
+public class CalculatorMain {
+	public static void main(String[] args) {
+		
+	}
+}
